916ba99e2210bba8331503b059b80f367a0b2a05
UBC Student <2860128966@qq.com>
2024-04-06 21:22:45 -0700

    login

--- auth.js
+++ auth.js
@@ -7,10 +7,14 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  pages: {
+    signIn: "/login",
+  },
   providers: [
     GoogleProvider({
         clientId:process.env.GOOGLE_CLIENT_ID,
         clientSecret:process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET
 })
---
+++ src/app/login/page.js
@@ -0,0 +1,55 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+
+import Image from "next/image";
+
+export default function Login(){
+  return (
+
+    <div className="flex flex-row">
+    <div className="w-1/2 h-screen flex flex-col justify-center items-center px-12">
+      <Image  
+          src="/assets/Logo.svg"
+          height={250}
+          width={250}
+          alt="logo"/>
+      <p className="pt-[180px]">
+        Outfit Connect: the ultimate app for outdoor enthusiasts! Snap and share your adventure outfits, and there will be a link to each item for easy shopping. Connect with fellow adventurers, inspire each other, and elevate your outdoor style. Gear up and explore like never before with Outfit Connect!
+      </p>
+          
+    </div>
+  
+   <div className="w-1/2 h-screen flex items-center justify-center bg-img relative">
+   <img class="background" src="/assets/bg-image.jpg" alt="Background Image"/>
+      <nav className="absolute top-0 left-0 right-0 z-50 flex flex-col items-end py-12 px-12">
+        <Button onClick={() => signIn("google")}>Login</Button>
+      </nav>
+
+
+      <div className="flex flex-col gap-5 w-full px-12 items-center">
+        <h1 className="text-xl font-bold text-center text-white">
+        Create an account
+        </h1>
+        <p className="text-center pb-10 text-white">
+         Signin with your Google account
+        </p>
+
+      <Button
+        size="lg"
+        className="w-1/2"
+        variant="outline"
+        onClick={() => signIn("google")}
+      >
+        <FcGoogle className="h-5 w-5" />
+      </Button>
+
+    
+      
+    </div>
+    </div>
+  </div>
+  
+  )
+}
